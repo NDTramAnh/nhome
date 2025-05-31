a25a58aa90a7b79ca6e9cac682b3811ae8d41dc4
ThienBao160 <22211TT3889@mail.tdc.edu.vn>
2025-05-31 20:24:50 +0700

    sua thong bao loi

--- resources/views/import/import.blade.php
+++ resources/views/import/import.blade.php
@@ -1,11 +1,7 @@
 @extends('home')
 
 @section('main-content')
-  @if (session('error'))
-    <div class="alert alert-danger">
-    {{ session('error') }}
-    </div>
-  @endif
+
 
   @if (session('success'))
     <div class="alert alert-success">
